21754542fdda78617f29faecf5ebdf58875ad258
Aman <amanmalik3105@gmail.com>
2023-01-11 17:55:05 +0530

    Initial Commit

---
+++ src/NewFile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NewFile.css"
+
+function NewFile(){
+    return (<div>
+        <h classname ="mainComponent">hello</h>
+        <p>World</p>
+    </div>);
+}
+
+
+export default NewFile;
